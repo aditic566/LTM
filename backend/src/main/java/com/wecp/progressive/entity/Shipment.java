6edcb3aa2e4d98abeb04ff6042cf46350d5656c5
username <wecp-dummy@gmail.com>
2025-01-08 12:47:49 +0000

    day10

--- backend/src/main/java/com/wecp/progressive/entity/Shipment.java
+++ backend/src/main/java/com/wecp/progressive/entity/Shipment.java
@@ -1,5 +1,107 @@
 package com.wecp.progressive.entity;
 
-public class Shipment {
+import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Shipment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int shipmentId;
+    private Date shipmentDate;
+    private Date expectedDeliveryDate;
+    private String sourceLocation;
+    private String destinationLocation;
+    private String status;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private Product product;
+ 
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private Warehouse warehouse;
+ 
+    public Shipment(){}
+
+    public Shipment(int shipmentId, Date shipmentDate, Date expectedDeliveryDate, String sourceLocation,
+            String destinationLocation, String status, Product product, Warehouse warehouse) {
+        this.shipmentId = shipmentId;
+        this.shipmentDate = shipmentDate;
+        this.expectedDeliveryDate = expectedDeliveryDate;
+        this.sourceLocation = sourceLocation;
+        this.destinationLocation = destinationLocation;
+        this.status = status;
+        this.product = product;
+        this.warehouse = warehouse;
+    }
+
+    public int getShipmentId() {
+        return shipmentId;
+    }
+
+    public void setShipmentId(int shipmentId) {
+        this.shipmentId = shipmentId;
+    }
+
+    public Date getShipmentDate() {
+        return shipmentDate;
+    }
+
+    public void setShipmentDate(Date shipmentDate) {
+        this.shipmentDate = shipmentDate;
+    }
+
+    public Date getExpectedDeliveryDate() {
+        return expectedDeliveryDate;
+    }
+
+    public void setExpectedDeliveryDate(Date expectedDeliveryDate) {
+        this.expectedDeliveryDate = expectedDeliveryDate;
+    }
+
+    public String getSourceLocation() {
+        return sourceLocation;
+    }
+
+    public void setSourceLocation(String sourceLocation) {
+        this.sourceLocation = sourceLocation;
+    }
+
+    public String getDestinationLocation() {
+        return destinationLocation;
+    }
+
+    public void setDestinationLocation(String destinationLocation) {
+        this.destinationLocation = destinationLocation;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Warehouse getWarehouse() {
+        return warehouse;
+    }
+
+    public void setWarehouse(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJpa.java
@@ -1,73 +1,63 @@
 package com.wecp.progressive.service.impl;
-
-
+ 
+ 
 import com.wecp.progressive.entity.Supplier;
 import com.wecp.progressive.exception.SupplierAlreadyExistsException;
 import com.wecp.progressive.exception.SupplierDoesNotExistException;
 import com.wecp.progressive.repository.ProductRepository;
+import com.wecp.progressive.repository.ShipmentRepository;
 import com.wecp.progressive.repository.SupplierRepository;
 import com.wecp.progressive.repository.WarehouseRepository;
 import com.wecp.progressive.service.SupplierService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+ 
 import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-
+ 
 @Service
 public class SupplierServiceImplJpa implements SupplierService {
-
-    @Autowired
-    WarehouseRepository warehouseRepository;
-
+ 
+    private final SupplierRepository supplierRepository;
+ 
     @Autowired
     ProductRepository productRepository;
-
-    private final SupplierRepository supplierRepository;
-
+ 
+    @Autowired
+    WarehouseRepository warehouseRepository;
+ 
     @Autowired
     public SupplierServiceImplJpa(SupplierRepository supplierRepository) {
         this.supplierRepository = supplierRepository;
     }
-
+ 
     @Override
     public List<Supplier> getAllSuppliers() throws SQLException {
         return supplierRepository.findAll();
     }
-
+ 
     @Override
     public int addSupplier(Supplier supplier) throws SQLException {
-        String name=supplier.getSupplierName();
-        String email=supplier.getEmail();
-        Optional<Supplier> newSupplier=supplierRepository.findBySupplierName(name);
-        Optional<Supplier> newEmail=supplierRepository.findByEmail(email);
-        if(newSupplier.isPresent() || newEmail.isPresent()){
-            throw new SupplierAlreadyExistsException("already exist supplier");
-        }
+        if(supplierRepository.findByEmail(supplier.getEmail()) != null && supplierRepository.findByUsername(supplier.getUsername()) != null)
+            throw new SupplierAlreadyExistsException("Supplier already exists with this email or username");
         return supplierRepository.save(supplier).getSupplierId();
-
     }
-
+ 
     @Override
     public List<Supplier> getAllSuppliersSortedByName() throws SQLException {
         List<Supplier> sortedSuppliers = supplierRepository.findAll();
         Collections.sort(sortedSuppliers);
         return sortedSuppliers;
     }
-
+ 
     @Override
     public void updateSupplier(Supplier supplier) throws SQLException {
-        String name=supplier.getSupplierName();
-        Optional<Supplier> newName=supplierRepository.findBySupplierName(name);
-        if(newName.isPresent()){
-            throw new SupplierAlreadyExistsException("already exists");
-        }
-        supplierRepository.save(supplier);
+            supplierRepository.save(supplier);
     }
-
+ 
     @Override
     @Transactional
     public void deleteSupplier(int supplierId) throws SQLException {
@@ -75,14 +65,13 @@ public class SupplierServiceImplJpa implements SupplierService {
         warehouseRepository.deleteBySupplierId(supplierId);
         supplierRepository.deleteBySupplierId(supplierId);
     }
-
+ 
     @Override
-    public Supplier getSupplierById(int supplierId) throws SQLException {
-        Optional<Supplier> optionalSupplier=supplierRepository.findBySupplierId(supplierId);
-        if(optionalSupplier.isPresent()){
-            throw new SupplierDoesNotExistException("doesnot exists");
-        }else{
-            return optionalSupplier.get();
+    public Supplier getSupplierById(int supplierId) throws SupplierDoesNotExistException {
+        Supplier supplier = supplierRepository.findBySupplierId(supplierId);
+        if (supplier != null) {
+            return supplierRepository.findBySupplierId(supplierId);
         }
+        throw new SupplierDoesNotExistException("Supplier with the given supplierId does not exists");
     }
 }
