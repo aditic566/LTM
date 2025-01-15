9f8ea3e0ea57b90fb3042b710e3a4f7708b2f3e0
username <wecp-dummy@gmail.com>
2025-01-15 11:59:34 +0000

    day16

--- frontend/src/app/supplylink/types/Supplier.ts
+++ frontend/src/app/supplylink/types/Supplier.ts
@@ -1,10 +1,29 @@
 export class Supplier {
-  
-
+ 
+    supllierId: number
+    supplierName: string
+    email: string
+    phone: string
+    address: string
+    username:string
+    password:string
+    role?: string
+ 
+    constructor(supplierId: number, supplierName: string, email:string, phone:string, address:string, username:string, password: string, role?:string ){
+        this.supllierId = supplierId
+        this.supplierName = supplierName
+        this.email = email
+        this.phone = phone
+        this.address = address
+        this.username= username
+        this.password = password
+        this.role = role
+    }
+    displayInfo() {
+        console.log(`Supplier ID: ${this.supllierId}`)
+        console.log(`Supplier name: ${this.supplierName}`)
+        console.log(`email: ${this.email}`)
+   
+    }
    
 }
-
-
-
-
-
--- frontend/src/app/supplylink/types/Warehouse.ts
+++ frontend/src/app/supplylink/types/Warehouse.ts
@@ -1,4 +1,22 @@
 import { Supplier } from "./Supplier";
 export class Warehouse {
- 
+    warehouseId:number;
+    supplierId:string;
+    warehouseName:string;
+    location:string;
+    capacity:number;
+
+    constructor(warehouseId:number,supplierId:string,warehouseName:string,location:string,capacity:number){
+        this.warehouseId=warehouseId;
+        this.supplierId=supplierId;
+        this.warehouseName=warehouseName;
+        this.location=location;
+        this.capacity=capacity;
+    }
+
+    displayInfo():void{
+        console.log(`Warehouse ID: ${this.warehouseId}
+Supplier ID: ${this.supplierId}
+Capacity: ${this.capacity}`);
+    }
 }
