37b38cee1e5bfeee59067fd8f2978adbe3124535
username <wecp-dummy@gmail.com>
2025-01-23 12:49:38 +0000

    day18

--- frontend/src/app/supplylink/components/supplier/supplier.component.ts
+++ frontend/src/app/supplylink/components/supplier/supplier.component.ts
@@ -1,5 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { Supplier } from '../../types/Supplier';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { of } from "rxjs";
 
-export class SupplierComponent  {
+@Component({
+  selector: 'app-supplier',
+  templateUrl: './supplier.component.html',
+  styleUrls: ['./supplier.component.scss']
+})
 
-  
+
+export class SupplierComponent implements OnInit {
+    supplierForm:FormGroup;
+    submitted:boolean | undefined;
+    supplierSuccess$:any;
+    supplierError$:any;
+    
+    constructor(private fb:FormBuilder){}
+
+    ngOnInit(): void {
+        this.supplierForm=this.fb.group({
+            name:['',[Validators.required]],
+            email:['',[Validators.required]],
+            username:['',[Validators.required]],
+            password:['',[Validators.required]]
+        });
+    }
+
+    onSubmit(){
+        this.submitted=true;
+        this.supplierSuccess$=of('');
+        this.supplierError$=of('');
+    }
 }
