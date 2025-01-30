205c6cdb73e390b6df4c992a1c2c9f54115b7853
username <wecp-dummy@gmail.com>
2025-01-30 13:20:56 +0000

    day19

--- frontend/src/app/supplylink/components/product/product.component.ts
+++ frontend/src/app/supplylink/components/product/product.component.ts
@@ -1,4 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Product } from "../../types/Product";
 
-export class ProductComponent {
- 
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.scss']
+  })
+export class ProductComponent implements OnInit{
+    productForm:any;
+    productNew:Product;
+    product:any;
+    submitted:boolean=false;
+    constructor(private fb:FormBuilder){}
+
+    ngOnInit(): void {
+        this.productForm=this.fb.group({
+            productId:[''], 
+            warehouseId:['',[Validators.required,Validators.min(1)]],
+            productName:['',Validators.required],
+            productDescription:[''], 
+            quantity:['',[Validators.required,Validators.min(0)]],
+            price:['',[Validators.required,Validators.min(1)]]
+        })
+        this.productNew=new Product(1,"2","xyz","xyz",10,10);
+    }
+
+    onSubmit():void{
+        if(this.productForm.valid){
+            this.submitted=true;
+            this.product=this.productForm.value;
+        }
+    }
 }
--- frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
+++ frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
@@ -1,5 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-export class WarehouseComponent {
- 
+@Component({
+    selector: 'app-warehouse',
+    templateUrl: './warehouse.component.html',
+    styleUrls: ['./warehouse.component.scss']
+  })
 
+export class WarehouseComponent implements OnInit{
+    warehouseForm:any;
+    
+    constructor(private fb:FormBuilder){}
+
+    ngOnInit():void{
+        this.warehouseForm=this.fb.group({
+            warehouseId:[''], 
+            supplierId:['',[Validators.required,Validators.min(1)]],
+            warehouseName:['',Validators.required],
+            location:[''],
+            capacity:['',[Validators.required,Validators.min(0)]]
+        });
+    }
+    onSubmit():void{        
+        throw new Error('Method not implemented.');
+    }
 }
